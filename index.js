874fa44e26d4329768c106215199528612523f94
17dchar <17dcharlet@gmail.com>
2021-12-22 22:02:15 -0600

    created command handler

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 // required classes and config
-const {Client, Intents} = require('discord.js');
+const fs = require('fs');
+const {Client, Collection, Intents} = require('discord.js');
 const {token} = require('./config.json');
 
-// intents
-/* not working
+// establish intents
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS);
-myIntents.add(Intents.FLAGS.GUILD_MEMBERS);
-myIntents.add(Intents.GUILD_MESSAGES);
-*/
+myIntents.add(Intents.FLAGS.GUILD_MESSAGES);
+
 
 // create client instance
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const client = new Client({intents: myIntents});
+client.commands = new Collection();
+
+// reads command files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// add commands from files to collection
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    // key = command name, value = exported module
+    client.commands.set(command.data.name, command);
+}
+
+// listen for command
+client.on('interactionCreate', async interaction => {
+    // ignore non-commands
+	if (!interaction.isCommand()) return;
+
+    // get respective command from list
+	const command = client.commands.get(interaction.commandName);
+    
+    // if command undefined => exit
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 // ready check
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Bee bot ready!');
 });
 
 client.login(token);
